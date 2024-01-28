656049095969760c669be678c5543bb957fbbcba
Bruce CHEN <805899926@qq.com>
2024-01-28 22:28:10 +0800

    # add server

---
+++ server/backend/src/main/java/com/cfbest/CFBestApplication.java
@@ -0,0 +1,12 @@
+package com.cfbest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CFBestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CFBestApplication.class, args);
+
+    }
+}
