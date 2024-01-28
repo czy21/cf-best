2030dc0d5de555349cf7748816e87ba408733d56
Bruce CHEN <805899926@qq.com>
2024-01-28 11:39:48 +0800

    # init scaner

--- scaner/main.py
+++ scaner/main.py
@@ -1,20 +1,20 @@
+import logging
 import os
 
 from telethon import TelegramClient, events
 
+logger = logging.getLogger()
 if __name__ == '__main__':
     api_id = int(os.getenv("CF_BEST_API_ID"))
     api_hash = os.getenv("CF_BEST_API_HASH")
     session = os.getenv("CF_BEST_SESSION")
     client = TelegramClient(session=session, api_id=api_id, api_hash=api_hash).start()
 
-
     @client.on(events.NewMessage(chats="@cf_push"))
     async def handler(event):
         print(event)
-
-
     try:
+        logger.info("cf-best-scaner started")
         client.run_until_disconnected()
     finally:
         client.disconnect()
