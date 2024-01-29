d9681f69fd69c8e09a4955de78ae6746a6150982
Bruce CHEN <805899926@qq.com>
2024-01-30 07:30:42 +0800

    # update

--- server/backend/src/main/java/com/cfbest/server/job/JobConfig.java
+++ server/backend/src/main/java/com/cfbest/server/job/JobConfig.java
@@ -2,9 +2,9 @@ package com.cfbest.server.job;
 
 import com.cfbest.server.feign.IpApiFeign;
 import com.cfbest.server.service.TelegramMessageService;
-import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDate;
 
@@ -19,7 +19,7 @@ public class JobConfig {
     @Autowired
     IpApiFeign ipApiFeign;
 
-    @XxlJob("processTGForCF")
+    @Scheduled(cron = "0 0/30 * * * ?")
     public void processTelegramMessageForCF() {
         telegramMessageService.process(LocalDate.now());
     }
