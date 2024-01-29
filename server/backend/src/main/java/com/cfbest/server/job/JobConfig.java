415dc75a78d71e2b4cda071dbea50a239cf03a59
Bruce CHEN <805899926@qq.com>
2024-01-29 20:47:59 +0800

    # update

--- server/backend/src/main/java/com/cfbest/server/job/JobConfig.java
+++ server/backend/src/main/java/com/cfbest/server/job/JobConfig.java
@@ -1,14 +1,12 @@
 package com.cfbest.server.job;
 
 import com.cfbest.server.feign.IpApiFeign;
-import com.cfbest.server.feign.model.IpApiBatchResult;
 import com.cfbest.server.service.TelegramMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Configuration
 public class JobConfig {
