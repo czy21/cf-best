f980c2e6728a09b152b7c9917f9a204fe8264219
Bruce CHEN <805899926@qq.com>
2024-03-18 22:59:24 +0800

    # sync consume message

--- scaner/main.py
+++ scaner/main.py
@@ -20,7 +20,7 @@ logger.addHandler(ch)
 logger.setLevel(log_level)
 if __name__ == '__main__':
     DBSession = sessionmaker(bind=sqlalchemy.create_engine(mysql_url, pool_recycle=3600))
-    client = TelegramClient(session=telethon_session, api_id=api_id, api_hash=api_hash).start()
+    client = TelegramClient(session=telethon_session, api_id=api_id, api_hash=api_hash, sequential_updates=True).start()
 
 
     @client.on(events.NewMessage(chats="@cf_push"))
@@ -29,19 +29,17 @@ if __name__ == '__main__':
         chat_id = event.message.chat.id
         message_id = event.message.id
         message_time = event.date.replace(tzinfo=pytz.utc).astimezone(pytz.timezone("Asia/Shanghai"))
-
-        if event.message.file is not None and event.message.file.ext == ".txt":
-            logger.debug("解析文件: {}".format(event.message))
-            file_blob = await client.download_media(event.media, bytes)
-            file_name = event.message.file.name
-            file_str = file_blob.decode('utf-8')
-            file_mime_type = event.message.file.mime_type
-            ips = file_str.splitlines() if file_str else []
-            with DBSession() as db_session:
+        with DBSession() as db_session:
+            if event.message.file is not None and event.message.file.ext == ".txt":
+                file_blob = await client.download_media(event.media, bytes)
+                file_name = event.message.file.name
+                file_str = file_blob.decode('utf-8')
+                file_mime_type = event.message.file.mime_type
+                ips = file_str.splitlines() if file_str else []
                 telegram_message_result = db_session.execute(
                     text("""INSERT INTO telegram_message(chat_id,message_id,message_time, type, content, file_name, file_type)
-                            values(:chat_id,:message_id,:message_time,:type,:content,:file_name,:file_type)
-                         """),
+                        values(:chat_id,:message_id,:message_time,:type,:content,:file_name,:file_type)
+                     """),
                     {
                         'chat_id': chat_id,
                         'message_id': message_id,
@@ -56,8 +54,8 @@ if __name__ == '__main__':
                 telegram_message_result_id = telegram_message_result.lastrowid
                 db_session.execute(
                     text("""INSERT INTO cf_cdn_ip(value_str,type,telegram_message_id) 
-                            values(:value_str,:type,:telegram_message_id)
-                         """),
+                        values(:value_str,:type,:telegram_message_id)
+                     """),
                     [
                         {
                             'value_str': t,
@@ -76,9 +74,7 @@ if __name__ == '__main__':
                     }
                 )
                 db_session.commit()
-        if event.message.raw_text is not None and event.message.raw_text.__contains__("扫描完毕"):
-            logger.debug("扫描完毕: {}".format(event.message))
-            with DBSession() as db_session:
+            if event.message.raw_text is not None and event.message.raw_text.__contains__("扫描完毕"):
                 db_session.execute(
                     text("update telegram_message set is_latest = 1 where chat_id = :chat_id and message_id = :message_id"),
                     {
